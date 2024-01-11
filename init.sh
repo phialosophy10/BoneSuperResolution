08002a86fa216e7c9065f5136635e5adb6228711
Sophia Wiinberg Bardenfleth <soeba@dtu.dk>
2024-01-11 13:40:15 +0100

    Removed folders from repo

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-module load python3/3.8.9
-module load numpy/1.19.5-python-3.8.9-openblas-0.3.13
-module load scipy/1.5.4-python-3.8.9
-module load cuda/11.3
-
-source halos/bin/activate
