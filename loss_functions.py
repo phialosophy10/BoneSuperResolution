2565fbe083c33c83b54a5458ee48b84b0e631197
Sophia Wiinberg Bardenfleth <soeba@dtu.dk>
2024-01-16 11:17:41 +0100

    Cleaned up files

--- loss_functions.py
+++ loss_functions.py
@@ -2,16 +2,15 @@
 import torch
 import torch.nn as nn
 from torchvision.models import vgg19
-import kornia as korn
-import torchio as tio
-import SR_config
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 ## Loss functions
 class VGGLoss(nn.Module):
     def __init__(self, layer_idx=35):
         super().__init__()
         self.layer_5_4 = layer_idx
-        self.vgg_model = vgg19(pretrained=True).features[:self.layer_5_4].eval().to(SR_config.DEVICE)
+        self.vgg_model = vgg19(pretrained=True).features[:self.layer_5_4].eval().to(DEVICE)
         self.loss = nn.MSELoss()
 
         for param in self.vgg_model.parameters():
@@ -27,14 +26,14 @@ class VGGLoss3D(nn.Module):
         super().__init__()
         self.layer_5_4 = layer_idx
 
-        self.vgg_model = vgg19(pretrained=True).features[:self.layer_5_4].eval().to(SR_config.DEVICE)
+        self.vgg_model = vgg19(pretrained=True).features[:self.layer_5_4].eval().to(DEVICE)
         self.loss_func = nn.MSELoss()
 
         for param in self.vgg_model.parameters():
             param.requires_grad = False
 
     def forward(self, real_hi_res, fake_hi_res):
-        loss = torch.zeros(1).to(SR_config.DEVICE)
+        loss = torch.zeros(1).to(DEVICE)
         for i in range(real_hi_res.shape[0]):
             real_patch = real_hi_res[i, :, :, :, :]
             fake_patch = fake_hi_res[i, :, :, :, :]
@@ -55,21 +54,3 @@ class VGGLoss3D(nn.Module):
         # Perhaps we need 1 more permute here to go back to the same patch orientation.
         return loss
 
-def performance_metrics(real_hi_res, fake_hi_res):
-    mse_func = nn.MSELoss()
-    rescale = tio.transforms.RescaleIntensity((0.0, 1.0))
-    psnr_total = 0
-    ssim_total = 0
-    L = 1.0  # Maximum value after scaling images between 0.0 and 1.0
-    
-    for real_patch, fake_patch in zip(real_hi_res, fake_hi_res):
-        real_patch_rescaled = rescale(real_patch.cpu()).unsqueeze(0)
-        fake_patch_rescaled = rescale(fake_patch.cpu()).unsqueeze(0)
-        mse = mse_func(real_patch_rescaled, fake_patch_rescaled)
-        psnr = 10*torch.log10((L**2)/mse)
-        psnr_total += psnr
-
-        ssim = torch.mean(korn.metrics.ssim3d(fake_patch_rescaled, real_patch_rescaled, window_size=11, max_val=L))
-        ssim_total += ssim
-
-    return psnr_total, ssim_total
